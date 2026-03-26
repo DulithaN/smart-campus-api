31d58bb442dc4294d216c846ad6a13d4f5219ba6
DulithaN <dulithaweerasinghe2@gmail.com>
2026-03-26 09:45:00 +0530

    feat: create RoomNotEmptyException for business constraint violation

---
+++ src/main/java/com/smartcampus/api/exception/RoomNotEmptyException.java
@@ -0,0 +1,15 @@
+﻿package com.smartcampus.api.exception;
+
+public class RoomNotEmptyException extends RuntimeException {
+    private String roomId;
+    private int sensorCount;
+    
+    public RoomNotEmptyException(String roomId, int sensorCount) {
+        super("Room " + roomId + " cannot be deleted as it contains " + sensorCount + " active sensor(s)");
+        this.roomId = roomId;
+        this.sensorCount = sensorCount;
+    }
+    
+    public String getRoomId() { return roomId; }
+    public int getSensorCount() { return sensorCount; }
+}
